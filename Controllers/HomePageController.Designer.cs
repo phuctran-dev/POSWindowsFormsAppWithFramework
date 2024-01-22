40292b6994b5ac48533fa7a72899deac79afdb46
phuc.tran <phuc.t@stor.ai>
2024-01-22 23:07:32 +0700

    Update UI

---
+++ Controllers/HomePageController.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace POSWindowsFormsAppWithFramework.Controllers
+{
+    partial class HomePageController
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbl_textView_Hello = new System.Windows.Forms.Label();
+            this.lbl_wrongUsername = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.Location = new System.Drawing.Point(481, 455);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(124, 29);
+            this.lbl_Username.TabIndex = 0;
+            this.lbl_Username.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(481, 541);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(611, 452);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 35);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(611, 538);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(249, 35);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lbl_textView_Hello
+            // 
+            this.lbl_textView_Hello.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_textView_Hello.Location = new System.Drawing.Point(431, 234);
+            this.lbl_textView_Hello.Name = "lbl_textView_Hello";
+            this.lbl_textView_Hello.Size = new System.Drawing.Size(493, 170);
+            this.lbl_textView_Hello.TabIndex = 4;
+            this.lbl_textView_Hello.Text = "Hello !";
+            this.lbl_textView_Hello.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_wrongUsername
+            // 
+            this.lbl_wrongUsername.AutoSize = true;
+            this.lbl_wrongUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_wrongUsername.ForeColor = System.Drawing.Color.Red;
+            this.lbl_wrongUsername.Location = new System.Drawing.Point(611, 494);
+            this.lbl_wrongUsername.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.lbl_wrongUsername.Name = "lbl_wrongUsername";
+            this.lbl_wrongUsername.Size = new System.Drawing.Size(211, 29);
+            this.lbl_wrongUsername.TabIndex = 5;
+            this.lbl_wrongUsername.Text = "Invalid Username !";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(611, 577);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Wrong Password!";
+            // 
+            // HomePageController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_wrongUsername);
+            this.Controls.Add(this.lbl_textView_Hello);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Username);
+            this.Name = "HomePageController";
+            this.Size = new System.Drawing.Size(1373, 865);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbl_textView_Hello;
+        private System.Windows.Forms.Label lbl_wrongUsername;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Forms/Form1.cs
@@ -0,0 +1,42 @@
+﻿using POSWindowsFormsAppWithFramework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSWindowsFormsAppWithFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HomePageController controller = new HomePageController();
+            addController(controller);
+        }
+
+        private void addController(UserControl control)
+        {
+            control.Dock = DockStyle.Fill;
+            panelContainer.Controls.Clear();
+            panelContainer.Controls.Add(control);
+            control.BringToFront();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_HomePage_Click(object sender, EventArgs e)
+        {
+            HomePageController controller = new HomePageController();
+            addController(controller);
+        }
+    }
+}
